0dd714cdb03440ec375ac3c552cfeea4a7ba0391
Alex Abdugafarov <fswork90@gmail.com>
2018-01-12 05:59:20 +0500

    Added forgotten MetaMessageTypes to git index

---
+++ TuxGuitar-midi/src/org/herac/tuxguitar/io/midi/base/MetaMessageTypes.java
@@ -0,0 +1,8 @@
+package org.herac.tuxguitar.io.midi.base;
+
+public class MetaMessageTypes {
+
+  public static final int TRACK_NAME            = 0x03;
+  public static final int TEMPO_CHANGE          = 0x51;
+  public static final int TIME_SIGNATURE_CHANGE = 0x58;
+}
